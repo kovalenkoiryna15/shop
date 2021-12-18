f0965f3cd297c69844913657545179ce36c0c844
Iryna Kavalenka <iryna_kavalenka@epam.com>
2021-12-18 16:22:06 +0300

    feat: create cart module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartModule } from './modules/cart/cart.module';
 import { ProductModule } from './modules/product/product.module';
 import { SharedModule } from './modules/shared/shared.module';
 
@@ -15,6 +16,7 @@ import { SharedModule } from './modules/shared/shared.module';
     AppRoutingModule,
     ProductModule,
     SharedModule,
+    CartModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
