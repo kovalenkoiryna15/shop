c00610986759eb72fe8af820e13d0e4948ac4da3
Iryna Kavalenka <iryna_kavalenka@epam.com>
2022-01-04 16:13:13 +0300

    feat: add highlight directive

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,6 +5,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatInputModule} from '@angular/material/input';
+import { HighlightDirective } from './directives/highlight.directive';
 
 const material = [
   MatCardModule,
@@ -21,6 +22,10 @@ const material = [
   ],
   exports: [
     ...material,
+    HighlightDirective,
+  ],
+  declarations: [
+    HighlightDirective
   ]
 })
 export class SharedModule {}
