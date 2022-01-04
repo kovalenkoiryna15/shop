73c44c8d250ef414db9cbe28b0fabebb47ef3a43
Iryna Kavalenka <iryna_kavalenka@epam.com>
2022-01-04 13:30:03 +0300

    feat: add app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import mockProducts from './mocks/mock.json';
 
@@ -7,10 +7,14 @@ import mockProducts from './mocks/mock.json';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'shop';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('appTitle') appTitle?: ElementRef<HTMLHeadingElement>;
 
   ngOnInit() {
     localStorage.setItem('products', JSON.stringify(mockProducts))
+  }
+
+  ngAfterViewInit() {
+    if (this.appTitle) this.appTitle.nativeElement.innerText = "Shop";
   }
 }
